4a5940cbb73aa9b9a944eaea6abc1e38bc03d436
maminnas <moin.aminnaseri@gmail.com>
2017-07-04 20:42:11 -0700

    used run_test.sh successfully and removed unnecessary files

--- run2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ./src/process_log.py ./sample_dataset/batch_log.json ./sample_dataset/stream_log.json ./log_output/flagged_purchases2.json
--- run3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ./src/process_log.py ./insight_testsuite/tests/test_2/log_input/batch_log.json ./insight_testsuite/tests/test_2/log_input/stream_log.json ./insight_testsuite/tests/test_2/log_output/flagged_purchases.json
